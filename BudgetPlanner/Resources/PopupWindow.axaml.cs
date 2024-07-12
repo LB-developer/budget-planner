01e04adb85213d9ba789fe9aa3105db55a13b170
Logan <thatsair@gmail.com>
2024-07-12 13:20:32 +1200

    add Frequency check
    ALSO:
      - Update styling

--- BudgetPlanner/Resources/PopupWindow.axaml.cs
+++ BudgetPlanner/Resources/PopupWindow.axaml.cs
@@ -9,27 +9,36 @@ namespace BudgetPlanner.Resources
 {
     public partial class PopupWindow : Window
     {   
-        string TransactionType = "";
+        bool TransactionFrequencySelected = false;
+        string TransactionFrequency = "";
         string TransactionValue = "";
       // Define an event to signal when a response is ready
         public event EventHandler<string>? ResponseReceived;
         public PopupWindow()
         {
             InitializeComponent();
+            this.Focus();
         }
         
         private void OnResponseInformation(object sender, RoutedEventArgs e)
         {
             
             TransactionValue = InputValue.Text ?? string.Empty;
+            if (!TransactionFrequencySelected)
+            {
+              
+              return;
+            }
+            
             if (TransactionValue == string.Empty)
             {
               InputValue.Watermark = "Value must be entered";
               return;
             }
-            
-            var type = TransactionType; // type of income/expense
-            var value = TransactionValue; // input value of income/expense
+            // type of income/expense
+            var type = TransactionFrequency; 
+            // input value of income/expense
+            var value = TransactionValue; 
             var response = $"{type},{value}";
             // Raise the event with the user's response
             ResponseReceived?.Invoke(this, response);
@@ -37,14 +46,14 @@ namespace BudgetPlanner.Resources
             // Close the popup window
             this.Close();
         }
-    
+
         private void WindowKeyDown(object sender, KeyEventArgs e)
         {
-          if (e.Key == Key.Enter)
+          if (e.Key == Key.Enter && TransactionFrequencySelected)
           {
             OnResponseInformation(sender, e);
           }
-          if (e.Key == Key.Back)
+          if (e.Key == Key.Escape)
           {
             ExitPressed(sender, e);
           }
@@ -54,7 +63,8 @@ namespace BudgetPlanner.Resources
           
           if (sender is Button button)
           {
-          TransactionType = button.Content?.ToString() ?? string.Empty;
+          TransactionFrequencySelected = true;
+          TransactionFrequency = button.Content?.ToString() ?? string.Empty;
           }
 
         }
